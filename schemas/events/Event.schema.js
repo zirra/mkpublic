a7da8cc7da63a9300b377e51672c12734fd5a2ad
zirra <msullivan@heysulli.com>
2019-08-05 08:44:02 -0500

    updates to logins

--- schemas/events/Event.schema.js
+++ schemas/events/Event.schema.js
@@ -3,10 +3,24 @@ const Schema = mongoose.Schema;
 const shortId = require('shortid');
 
 const EventSchema = Schema({
-  dbid: {
+  myid: {
     type: String,
     default: shortId.generate,
     index: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  sdate: {
+    type: Date,
+    default: null,
+    required: true
+  },
+  edate: {
+    type: Date,
+    default: null,
+    required: true
   }
 },
 {
@@ -21,9 +35,30 @@ class EventItem {
   static async AddItem (data) {
     try {
       let result = await this.create(data);
-      return result;
+      return result
     } catch (error) {
-      return error;
+      return error
+    }
+  }
+
+  static async GetItems () {
+    try {
+      let result = await this.find()
+        .exec();
+        return result
+      } catch (error) {
+        return error
+      }
+  }
+
+  static async getIem (id) {
+    let {myid} = id
+    try {
+      let result = await this.findOne({myid})
+        .exec()
+      return result
+    } catch (error) {
+      return error
     }
   }
 }
